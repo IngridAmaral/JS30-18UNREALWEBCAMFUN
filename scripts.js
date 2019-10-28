aa9547e8d7f9f7b20b8400cd20a01d0786cc37b7
IngridAmaral <52465213+IngridAmaral@users.noreply.github.com>
2019-10-28 14:06:18 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -26,8 +26,16 @@ function paintToCanvas(){
 }
 
 function takePhoto(){
+    //played sound
     snap.currentTime = 0;
-    snap.play()
+    snap.play();
+
+    //take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg'); //if you console it will be a long text with atributes about the photo
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    strip.insertBefore(link, strip.firstChild); //insert the link it's going to happen rigth before the strip first child
 }
 
 getVideo();
