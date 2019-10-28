08241658f8ba75f5a93a1be7be0104ba723751ed
IngridAmaral <52465213+IngridAmaral@users.noreply.github.com>
2019-10-28 13:40:56 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,3 +3,33 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+            .then(localMediaStream => {
+                //this won't work  ->  video.src = window.URL.createObjectURL(localMediaStream);
+                video.srcObject = localMediaStream;
+                video.play();
+            })
+            .catch(err => console.error('OH NO!', err))
+}
+
+function paintToCanvas(){
+    const height = video.videoHeight;
+    const width = video.videoWidth;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+
+function takePhoto(){
+    snap.currentTime = 0;
+    snap.play()
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
